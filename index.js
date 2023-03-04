fe541c154be53b9a770d1b9dbff2250e2a336fe1
itlleat <scott.schulman84@gmail.com>
2023-03-04 14:27:08 -0700

    merge conflict

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
-const inquirer = requirer('inquirer')
-const fs = require('fs')
-const jest = require('jest')
+const inquirer = require('inquirer');
+const fs = require('fs');
+const jest = require('jest');
 
 
